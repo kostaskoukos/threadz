d0c192248e2657e770474b021ebdcb951f905c35
Kostas Koukos <kkonst2014@gmail.com>
2023-10-29 16:59:45 +0200

    removed edgeMiddleware

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,5 @@ import vercel from "@astrojs/vercel/serverless";
 
 export default defineConfig({
   output: "server",
-  adapter: vercel({
-    edgeMiddleware: true,
-  }),
+  adapter: vercel(),
 });
