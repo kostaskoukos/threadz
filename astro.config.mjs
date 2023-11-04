f4521ae8b08add3073827089e6f13880e27795df
Kostas Koukos <kkonst2014@gmail.com>
2023-11-04 16:23:28 +0200

    added shadcn ui config

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,5 +8,10 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   output: "server",
   adapter: vercel(),
-  integrations: [react(), tailwind()]
+  integrations: [
+    react(),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+  ],
 });
